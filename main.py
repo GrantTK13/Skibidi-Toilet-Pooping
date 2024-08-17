aa046405f96322b9130177ec3cb76b4d438d52c8
GrantTK13 <147281972+GrantTK13@users.noreply.github.com>
2024-08-16 21:15:47 -0400

    skibidi poop file

---
+++ main.py
@@ -0,0 +1,19 @@
+import time
+
+print("                   ___")
+print("          ____    |   |")
+print("         /| | \   |   |")
+print("         \ __ /   |   |")
+print("       ___|__|____|   |")
+print("       \          |   |")
+print("        \_________|___|")
+print("             |   |")
+print("           __|   |__")
+print("          /_________\  ")
+
+while True:
+    time.sleep(2)
+    print("                _")
+    print("              _|_|_")
+    print("            _|_____|_")
+    print("           |_________|")
